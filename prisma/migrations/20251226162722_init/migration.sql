2e90ce9238c7217fce86f8e5a8626a9ba0e4d9bd
zakizakaria99 <zakizakariazakse@gmail.com>
2025-12-26 23:30:35 +0700

    chore: resolve prisma migration after db push

--- prisma/migrations/20251226162722_init/migration.sql
+++ prisma/migrations/20251226162722_init/migration.sql
@@ -1,3 +1,12 @@
+/*
+  Warnings:
+
+  - You are about to alter the column `price` on the `Product` table. The data in that column could be lost. The data in that column will be cast from `Decimal(10,2)` to `Integer`.
+
+*/
+-- AlterTable
+ALTER TABLE "Product" ALTER COLUMN "price" SET DATA TYPE INTEGER;
+
 -- CreateTable
 CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
